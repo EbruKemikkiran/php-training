c890f01ebddc1ab56f0161d9e1610dde98a4d5d9
EbruKemikkiran <ebrukemikkiran@hotmail.com>
2024-03-06 02:58:04 +0000

    saved

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" />
+    <title>Document</title>
+</head>
+<body>
+    
+
+<nav class="nav.navbar.navbar-expand-lg bg-danger navbar-dark">
+    <div class="container">
+        <a href="/" class="navbar-brand">PHP Course 2024</a>
+        <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+                <a href="#" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Contact</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">FAQ</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container">
+    <div class="row"></div>
+        <div class="col-4">
+            <div class="list-group">
+                <a href="#" class="list-group-item list-group-item-action">Software Developer</a>
+                <a href="#" class="list-group-item list-group-item-action">Web Developer</a>
+                <a href="#" class="list-group-item list-group-item-action">Mobile Developer</a>
+
+            </div>
+
+        </div>
+        <div class="col-9">
+            <div class="card mb-3">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="img/project-img4.jpg" alt="" class="img-fluid rounded-start">
+                    </div>
+                    <div class="com-md-8">
+                        <h5 class="card-title">Web Developing</h5>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque incidunt corporis aliquam id amet quo adipisci a voluptas officia cupiditate enim mollitia suscipit, consequatur rem nihil natus unde eos totam!
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-9">
+            <div class="card mb-3">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="img/project-img1.jpg" alt="" class="img-fluid rounded-start">
+                    </div>
+                    <div class="com-md-8">
+                        <h5 class="card-title">Software Developing</h5>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque incidunt corporis aliquam id amet quo adipisci a voluptas officia cupiditate enim mollitia suscipit, consequatur rem nihil natus unde eos totam!
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-9">
+            <div class="card mb-3">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="img/projecdt-img2.jpg" alt="" class="img-fluid rounded-start">
+                    </div>
+                    <div class="com-md-8">
+                        <h5 class="card-title">Mobile Developing</h5>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque incidunt corporis aliquam id amet quo adipisci a voluptas officia cupiditate enim mollitia suscipit, consequatur rem nihil natus unde eos totam!
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+
+
+
+
+
+</body>
+</html>
